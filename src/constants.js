b44eadef6ccb48426160cf5ff98a261e17b4d325
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-13 13:45:30 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,17 @@
-export const WARNING_IMAGE_VERSION = `WARNING source.md has ${index} ${element}. Replace it with memes`;
+export const WARNING_IMAGE_VERSION  = `WARNING source.md has ${index} ${element}. Replace it with memes`;
 export const ERROR_SOURCE_DONT_HAVE = `ERROR source.md doesn't have ${error}`;;
 
-export const FULL_TEMPLATE_ERROR = 'The full template has not been parsed!';
-export const HTML_EMAIL_SUCCESS = Content has correct html!!!;
-export const HTML_EMAIL_ERROR = 'Content has not correct html!!!';    
+export const FULL_TEMPLATE_ERROR  = 'The full template has not been parsed!';
+export const HTML_EMAIL_SUCCESS   = 'Content has correct html!!!';
+export const HTML_EMAIL_ERROR     = 'Content has not correct html!!!';    
+
+// ---
+
+
+export const CONST_FILE_CREATED = '';
+export const CONST_FILE_NOT_WRITTEN = 'file not written';
+
+export const ERROR_NO_TITLE = '`title` is a required option for `renderTemplate`';
+export const ERROR_NO_BODY_CONTENT = '`bodyContent` is a required option for `renderTemplate`';
+export const ERROR_NO_PREVIEW_TEXT = '`previewText` is a required option for `renderTemplate`';
+
