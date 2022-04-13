1589325bec7d4d9dab51e966ba334b3cf02d9957
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-13 13:22:41 +0300

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const WARNING_IMAGE_VERSION = `WARNING source.md has ${index} ${element}. Replace it with memes`;
+export const ERROR_SOURCE_DONT_HAVE = `ERROR source.md doesn't have ${error}`;;
+
+export const FULL_TEMPLATE_ERROR = 'The full template has not been parsed!';
+export const HTML_EMAIL_SUCCESS = Content has correct html!!!;
+export const HTML_EMAIL_ERROR = 'Content has not correct html!!!';    
