b03878ce66eb2c134d06b073578ec0064465c0b3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-09 02:33:26 +0300

    Create write.js

---
+++ src/write.js
@@ -0,0 +1,18 @@
+import generateTemplateName from './utils';
+
+const writingFile = (content, name) => {
+  if (!content) {
+    throw new Error('no content was passed into writingFile method');
+  }
+  
+  const fileName = generateTemplateName(name);
+
+  try {
+    writeHTML(fileName, content);
+  } catch (err) {
+    //console.log(err);
+    //console.log(content);
+  }
+};
+
+export { writingFile };
