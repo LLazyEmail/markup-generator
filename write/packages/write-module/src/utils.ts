c8170361ca332bd29a1cc29ef9316ccf096430e9
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2023-02-28 12:36:24 +0000

    123

--- write/packages/write-module/src/utils.ts
+++ write/packages/write-module/src/utils.ts
@@ -2,11 +2,13 @@
 const catchErrorTraceOutput = (error:any) => {
     // we need to test how it actually work
     const callerLine = error.stack.split('\n')[4];
-    const index = callerLine.indexOf('at ');
-    // eslint-disable-next-line no-unused-vars
-    const clean = callerLine.slice(index + 2, callerLine.length);
-  
-    throw error;
+    if(callerLine){
+        const index = callerLine.indexOf('at ');
+        // eslint-disable-next-line no-unused-vars
+        const clean = callerLine.slice(index + 2, callerLine.length);
+      
+        throw error;
+    }    
 };
 
 
