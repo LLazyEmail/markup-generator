ae91dde276416afb7dc8f58415226116f5bd80cb
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2026-09-13 06:13:49 +0300

    fix: use jest.config.cjs so CI does not need ts-node

---
+++ jest.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  displayName: 'markup-generator',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          target: 'ES2020',
+          module: 'commonjs',
+          esModuleInterop: true,
+          strict: true,
+          types: ['jest', 'node'],
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: [
+    '<rootDir>/src/**/*.(spec|test).ts',
+    '<rootDir>/tests/**/*.(spec|test).ts',
+  ],
+};
