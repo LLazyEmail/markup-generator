6abc4c7d2e9a1d861690554d7c21c3b3177a353f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2026-09-17 03:13:03 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ import { writeGeneratedEmail } from './writeGeneratedEmail';
 
 export type { WriteGeneratedFileOptions } from './types';
 export type { WriteGeneratedEmailOptions } from './writeGeneratedEmail';
+
+export { readContentFile, loadDataModule, resolveFromCwd } from './read';
 export { MarkupGeneratorError } from './errors';
 export type { MarkupGeneratorErrorCode } from './errors';
 
